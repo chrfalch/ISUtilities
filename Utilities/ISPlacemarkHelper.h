ef7ec7af1dd3ae1fd2a6516d9e8bce473953ff55
Christian Falch <christian.falch@gmail.com>
2013-03-08 21:23:08 +0100

    Added placemark helper.

---
+++ Utilities/ISPlacemarkHelper.h
@@ -0,0 +1,44 @@
+//
+//  ISPlacemarkHelper.h
+//  TripLog
+//
+//  Created by Christian Falch on 3/8/13.
+//  Copyright (c) 2013 Islandssoftware. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <MapKit/MapKit.h>
+
+@interface ISPlacemarkHelper : NSObject
+
+// Gets a two-line address separated with comma
++(NSString*) addressFromPlacemark:(MKPlacemark*)placemark;
+
+// Returns a correctly formatted address line from an address dictionary
++(NSString*) addressFromPlacemark:(MKPlacemark*)placemark forFirstLine:(BOOL)firstLine;
+
+// Returns the street name from a placemark without street number
++(NSString*) streetNameFromPlacemark:(MKPlacemark*)placemark;
+
+// Updates the address dictionary with name or details
++(void) setAddressForPlacemark:(MKPlacemark*)placemark address:(NSString*)address forFirstLine:(BOOL)firstLine;
+
+// Returns true if the location has a coordinate
++(BOOL) placemarkHasCoordinate:(MKPlacemark*)placemark;
+
+// returns true if the placemark has a valid address
++(BOOL) placemarkHasAddress:(MKPlacemark*)placemark;
+
+// creates a copy from a dictonary and a coordinate
++(MKPlacemark*) placemarkFromCoordinate:(CLLocationCoordinate2D)coordinate andAddressDictionary:(NSDictionary*)addressDictionary;
+
+// Returns a dictionary formatted to be used in a placemark
++(NSMutableDictionary*) dictionaryWithAddress:(NSString*)address withDetails:(NSString*)details;
+
+// Performs a deep copy of a placemark
++(MKPlacemark*) placemarkDeepCopyFromPlacemark:(MKPlacemark*)placemark;
+
+// Creates a new placemark with the given values
++(MKPlacemark*) placemarkWithCoordinate:(CLLocationCoordinate2D)coordinate name:(NSString*)name details:(NSString*)details;
+
+@end
---
+++ Utilities/ISPlacemarkHelper.m
@@ -0,0 +1,229 @@
+//
+//  ISPlacemarkHelper.m
+//  TripLog
+//
+//  Created by Christian Falch on 3/8/13.
+//  Copyright (c) 2013 Islandssoftware. All rights reserved.
+//
+
+#import "ISPlacemarkHelper.h"
+
+@implementation ISPlacemarkHelper
+
++(MKPlacemark*) placemarkDeepCopyFromPlacemark:(MKPlacemark *)placemark
+{
+    NSDictionary* addressDictionary = [[NSMutableDictionary alloc] initWithCapacity:1];
+    NSMutableArray* addressLines = [[NSMutableArray alloc] init];
+    [addressLines addObject:[ISPlacemarkHelper addressFromPlacemark:placemark forFirstLine:YES]];
+    [addressLines addObject:[ISPlacemarkHelper addressFromPlacemark:placemark forFirstLine:NO]];
+    [addressDictionary setValue:addressLines forKey:@"FormattedAddressLines"];
+    
+    return [[MKPlacemark alloc] initWithCoordinate:CLLocationCoordinate2DMake(placemark.coordinate.latitude, placemark.coordinate.longitude) addressDictionary:addressDictionary];
+}
+
++(MKPlacemark*) placemarkFromCoordinate:(CLLocationCoordinate2D)coordinate andAddressDictionary:(NSDictionary*)addressDictionary
+{
+    // get address lines
+    NSArray* addressLines = [addressDictionary objectForKey:@"FormattedAddressLines"];
+    
+    // Create copies
+    NSMutableDictionary* addressDictionaryCopy = [[NSMutableDictionary alloc] initWithCapacity:1];
+    NSMutableArray* addressLinesCopy = [[NSMutableArray alloc] init];
+    
+    [addressLinesCopy addObject:[addressLines objectAtIndex:0]];
+    [addressLinesCopy addObject:[addressLines objectAtIndex:1]];
+    [addressDictionaryCopy setValue:addressLinesCopy forKey:@"FormattedAddressLines"];
+    
+    return [[MKPlacemark alloc] initWithCoordinate:CLLocationCoordinate2DMake(coordinate.latitude, coordinate.longitude) addressDictionary:addressDictionaryCopy];
+}
+
+// Returns a dictionary formatted to be used in a placemark
++(NSMutableDictionary*) dictionaryWithAddress:(NSString*)address withDetails:(NSString*)details
+{
+    if(address == nil)
+        address = @"";
+    
+    if(details == nil)
+        details = @"";
+    
+    NSMutableDictionary* addressDictionary = [[NSMutableDictionary alloc] initWithCapacity:1];
+    NSMutableArray* addressLines = [[NSMutableArray alloc] init];
+    [addressLines addObject:address];
+    [addressLines addObject:details];
+    [addressDictionary setValue:addressLines forKey:@"FormattedAddressLines"];
+    return addressDictionary;
+}
+
+// Creates a new placemark with the given values
++(MKPlacemark*) placemarkWithCoordinate:(CLLocationCoordinate2D)coordinate name:(NSString*)name details:(NSString*)details
+{
+    if(name == nil)
+        name = @"";
+    
+    if(details == nil)
+        details = @"";
+    
+    NSMutableDictionary* addressDictionary = [[NSMutableDictionary alloc] initWithCapacity:1];
+    NSMutableArray* addressLines = [[NSMutableArray alloc] init];
+    [addressLines addObject:name];
+    [addressLines addObject:details];
+    [addressDictionary setValue:addressLines forKey:@"FormattedAddressLines"];
+    
+    return [[MKPlacemark alloc] initWithCoordinate:CLLocationCoordinate2DMake(coordinate.latitude, coordinate.longitude) addressDictionary:addressDictionary];
+}
+
+// Updates the address dictionary with name or details
++(void) setAddressForPlacemark:(MKPlacemark*)placemark address:(NSString*)address forFirstLine:(BOOL)firstLine
+{
+    if([self placemarkHasAddress:placemark])
+    {
+        NSMutableArray* array = [[placemark addressDictionary] objectForKey:@"FormattedAddressLines"];
+        
+        // ensure element count = 2
+        while(array.count < 2)
+            [array addObject:@""];
+        
+        if(firstLine)
+        {
+            [array removeObjectAtIndex:0];
+            [array insertObject:address atIndex:0];
+        }
+        else {
+            [array removeObjectAtIndex:1];
+            [array addObject:address];
+        }
+    }
+    else
+    {
+        dNSLog(@"placemark does not have address!");
+    }
+}
+
++(NSString*) streetNameFromPlacemark:(MKPlacemark*)placemark
+{
+    // First, check if we have a nil value. Just return information on what to di
+    if(!placemark)
+        return NSLocalizedString(@"LblNoLocationSelected", nil);
+    
+    // If we have address information
+    if([self placemarkHasAddress:placemark])
+    {
+        
+        NSString* street = [[placemark addressDictionary] objectForKey:@"Street"];
+        if(street && street.length > 0)
+        {
+            NSArray* streetComps = [street componentsSeparatedByCharactersInSet:[NSCharacterSet characterSetWithCharactersInString:@"- ,"]];
+            
+            NSMutableString* streetWithOutNumbers = [[NSMutableString alloc] init];
+            NSNumberFormatter* ns = [[NSNumberFormatter alloc] init];
+            
+            for(NSString* s in streetComps)
+            {
+                // Does the component begin with a number?
+                BOOL beginsWithNumber = [s length] && isnumber([s characterAtIndex:0]);
+                
+                if(beginsWithNumber || [ns numberFromString:s])
+                    continue;
+                
+                // Not a number and does not start with a number
+                [streetWithOutNumbers appendFormat:@"%@%@", (streetWithOutNumbers.length > 0 ? @" " : @""), s];
+            }
+            
+            street = [NSString stringWithString:streetWithOutNumbers];
+        }
+        
+        if(street && street.length > 0)
+            return street;
+        else
+            return [self addressFromPlacemark:placemark];
+        
+    }
+    else if([self placemarkHasCoordinate:placemark])
+    {
+        NSString *latString = (placemark.coordinate.latitude < 0) ? NSLocalizedString(@"South", nil) : NSLocalizedString(@"North", nil);
+        
+        NSString *lonString = (placemark.coordinate.longitude < 0) ? NSLocalizedString(@"West", nil) : NSLocalizedString(@"East", nil);
+        
+        return [NSString stringWithFormat:NSLocalizedString(@"LatLongFormat", nil), fabs(placemark.coordinate.latitude), latString, fabs(placemark.coordinate.longitude), lonString];
+    }
+    else
+    {
+        
+        return NSLocalizedString(@"LblNoLocationSelected", nil);
+    }
+    
+}
+
++(NSString*) addressFromPlacemark:(MKPlacemark*)placemark
+{
+    return [NSString stringWithFormat:@"%@, %@", [self addressFromPlacemark:placemark forFirstLine:YES], [self addressFromPlacemark:placemark forFirstLine:NO]];
+}
+
+// Returns a correctly formatted address line from an address dictionary
++(NSString*) addressFromPlacemark:(MKPlacemark*)placemark forFirstLine:(BOOL)firstLine
+{
+    // First, check if we have a nil value. Just return information on what to di
+    if(!placemark)
+    {
+        if(firstLine)
+            return NSLocalizedString(@"LblNoLocationSelected", nil);
+        else
+            return NSLocalizedString(@"LblNoLocationSelectedClickToSet", nil);
+    }
+    
+    // If we have address information
+    if([self placemarkHasAddress:placemark])
+    {
+        if(firstLine)
+        {
+            return [[placemark.addressDictionary valueForKey:@"FormattedAddressLines"] objectAtIndex:0];
+        }
+        else
+        {
+            NSArray* arr = (NSArray*)[placemark.addressDictionary valueForKey:@"FormattedAddressLines"];
+            
+            NSRange range;
+            range.location = 1;
+            range.length = [arr count]-1;
+            
+            
+            NSArray* tmp = [[NSArray alloc] initWithArray: [arr subarrayWithRange:range]];
+            
+            return [tmp componentsJoinedByString:@", "];
+        }
+    }
+    else if([self placemarkHasCoordinate:placemark])
+    {
+        NSString *latString = (placemark.coordinate.latitude < 0) ? NSLocalizedString(@"South", nil) : NSLocalizedString(@"North", nil);
+        
+        NSString *lonString = (placemark.coordinate.longitude < 0) ? NSLocalizedString(@"West", nil) : NSLocalizedString(@"East", nil);
+        
+        if(firstLine)
+            return [NSString stringWithFormat:NSLocalizedString(@"LatLongFormat", nil), fabs(placemark.coordinate.latitude), latString, fabs(placemark.coordinate.longitude), lonString];
+        else
+            return @"";
+        
+    }
+    else
+    {
+        
+        if(firstLine)
+            return NSLocalizedString(@"LblNoLocationSelected", nil);
+        else
+            return NSLocalizedString(@"LblNoLocationSelectedClickToSet", nil);
+    }
+}
+
++(BOOL) placemarkHasCoordinate:(MKPlacemark*)placemark
+{
+    return placemark && placemark.coordinate.latitude != 0.0 && placemark.coordinate.longitude != 0.0;
+}
+
+// returns true if the placemark has a valid address
++(BOOL) placemarkHasAddress:(MKPlacemark*)placemark
+{
+    return placemark && placemark.addressDictionary && [placemark.addressDictionary allKeys].count > 0 && [placemark.addressDictionary valueForKey:@"FormattedAddressLines"] != nil && ![[((NSArray*)[placemark.addressDictionary valueForKey:@"FormattedAddressLines"]) componentsJoinedByString:@""] isEqualToString:@""];
+}
+@end
+
+
